850cae845605044d4ddf5af9b6da9dc55c50b3df
Cramer-Lawrence <cramer.lawrence@msn.com>
2025-04-14 16:23:51 -0600

    Clang Formatted timer.cpp

--- libbench/src/timer.cpp
+++ libbench/src/timer.cpp
@@ -2,17 +2,13 @@
 
 namespace libbench {
 
-	Timer::Timer() {
-		reset();
-	}
+Timer::Timer() { reset(); }
 
-	void Timer::reset() {
-		start_ = std::chrono::high_resolution_clock::now();
-	}
+void Timer::reset() { start_ = std::chrono::high_resolution_clock::now(); }
 
-	double Timer::elapsed() const {
-		auto now = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> diff = now - start_;
-		return diff.count();
-	}
+double Timer::elapsed() const {
+  auto now = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diff = now - start_;
+  return diff.count();
 }
+} // namespace libbench
