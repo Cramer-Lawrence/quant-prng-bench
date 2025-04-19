5cb11747f55c5be100d33fbf8f36d21dcffb0a1d
Cramer-Lawrence <cramer.lawrence@msn.com>
2025-04-19 12:04:45 -0600

    cleaned up the benchstatics class

--- libbench/include/BenchStatics.h
+++ libbench/include/BenchStatics.h
@@ -1,44 +1,21 @@
 #ifndef BENCHSTATICS_H
 #define BENCHSTATICS_H
 
-#include <string>
-#include <vector>
-#include <utility>
 #include <cstdint>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
-class BenchStatics
-{
+class BenchStatics {
 public:
-	static std::vector<std::pair<uint64_t, int>> calculateDistribution(std::vector<uint64_t> inVec, const std::string& reportTypeMsg);
+  static std::unordered_map<uint64_t, int>
+  calculateDistribution(const std::vector<uint64_t> &inVec,
+                        const std::string &reportTypeMsg);
 
-	static void generateDistributionRatio(std::vector<std::pair<uint64_t, int>> distVec, const std::string& reportTypeMsg);
-
-private:
-	template<typename T>
-	static int checkVectorForValue(std::vector<T>& checkVector, const T& searchValue)
-	{
-		for (int i = 0; i < checkVector.size(); ++i)
-		{
-			if (checkVector[i] == searchValue)
-			{
-				return i;
-			}
-		}
-		return -1;
-	}
-
-	template<typename T, typename U>
-	static int checkVectorForValue(std::vector<std::pair<T, U>> checkVector, const T& searchValue)
-	{
-		for (int i = 0; i < checkVector.size(); ++i)
-		{
-			if (checkVector[i].first == searchValue)
-			{
-				return i;
-			}
-		}
-		return -1;
-	}
+  static void
+  generateDistributionRatio(std::unordered_map<uint64_t, int>& frequencyMap,
+                            const std::string &reportTypeMsg);
 };
 
 #endif
--- libbench/src/BenchStatics.cpp
+++ libbench/src/BenchStatics.cpp
@@ -1,53 +1,50 @@
 #include "BenchStatics.h"
 
-#include <bits/stdc++.h>
+#include <algorithm>
 
 #include "BenchReport.h"
 
+std::unordered_map<uint64_t, int>
+BenchStatics::calculateDistribution(const std::vector<uint64_t> &inVec,
+                                    const std::string &reportTypeMsg) {
 
-std::vector<std::pair<uint64_t, int>> BenchStatics::calculateDistribution(std::vector<uint64_t> inVec, const std::string& reportTypeMsg) {
-  
-	std::vector<std::pair<uint64_t, int>> outVec{};
-	
-	BenchReport::log("Generating " + reportTypeMsg + " Distribution Report...");
+  std::unordered_map<uint64_t, int> frequencyMap;
+  BenchReport::log("Generating " + reportTypeMsg + " Distribution Report...");
 
   for (const uint64_t &i : inVec) {
-    int n { checkVectorForValue(outVec, i) };
-    if ( n == -1)
-    {
-      outVec.push_back({i, 1});
-    }
-    else
-    {
-      outVec[n].second++;
-    }
+    ++frequencyMap[i];
   }
 
-  std::sort(outVec.begin(), outVec.end());
-
-  for (std::pair<const int, int> p : outVec) {
-    BenchReport::log(std::to_string(p.first) 
-		    + " - " 
-		    + std::to_string(p.second));
+  for (const auto &p : frequencyMap) {
+    BenchReport::log(std::to_string(p.first) + " - " +
+                     std::to_string(p.second));
   }
 
   BenchReport::log("Distribution Report Complete");
 
-  return outVec;
+  return frequencyMap;
 }
 
-void BenchStatics::generateDistributionRatio(std::vector<std::pair<uint64_t, int>> distVec, const std::string& reportTypeMsg)
-{
-  double min {2e64};
-  double max {0};
-  
-  for (const std::pair<uint64_t, int>& p : distVec)
-  {
-    if (p.second < min)
-	    min = p.second;
-    if (p.second > max)
-	    max = p.second;
+void BenchStatics::generateDistributionRatio(
+    std::unordered_map<uint64_t, int> &frequencyMap,
+    const std::string &reportTypeMsg) {
+
+  if (frequencyMap.empty()) {
+    BenchReport::log("Invalid Distribution: frequency map is empty");
+    return;
   }
 
-  BenchReport::log("Distribution Ratio for " + reportTypeMsg + ": " + std::to_string(min / max) + " (the closer to 1 the better)\n");
+  auto [min, max] = std::minmax_element(
+      frequencyMap.begin(), frequencyMap.end(),
+      [](const auto &a, const auto &b) { return a.second < b.second; });
+
+  if (max->second == 0) {
+    BenchReport::log("Invalid Distribution: max has frequency of 0");
+    return;
+  }
+
+  double ratio = static_cast<double>(min->second) / max->second;
+
+  BenchReport::log("Distribution Ratio for " + reportTypeMsg + ": " +
+                   std::to_string(ratio) + " (the closer to 1 the better)\n");
 }
